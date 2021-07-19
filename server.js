07a959c734d943085959f7d9fa890d0da881a50a
DESKTOP-AQ46HRP\Par <vaslysalex@hotmail.gr>
2021-07-19 21:23:08 +0300

    second

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const initRoutes = require("./routes");
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = 8000;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
